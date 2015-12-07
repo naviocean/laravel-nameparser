1e43f52667a1ccfc29888016624f2e0429076cb9
Duong Le <naviocean@Duongs-MacBook-Pro.local>
2015-12-07 21:01:05 +0700

    fix static class

--- src/NaviOcean/Laravel/NameParser.php
+++ src/NaviOcean/Laravel/NameParser.php
@@ -16,7 +16,7 @@ class NameParser {
    *  - Common compound surname identifiers
    *  - Common suffixes (lineage and professional)
    */
-  protected $dict = array(
+  protected static $dict = array(
     'prefix' => array(
       'Mr.' => array('mr', 'mister', 'master'),
       'Mrs.' => array('mrs', 'missus', 'missis'),
@@ -42,27 +42,27 @@ class NameParser {
    * @param string $name the full name you wish to parse
    * @return array returns associative array of name parts
    */
-  public function parse_name($full_name) {
+  public static function parse_name($full_name) {
     # Remove leading/trailing whitespace
     $full_name = trim($full_name);
     # Setup default vars
     extract(array('salutation' => '', 'fname' => '', 'initials' => '', 'lname' => '', 'suffix' => ''));
     # If name contains professional suffix, assign and remove it
-    $professional_suffix = $this->get_pro_suffix($full_name);
+    $professional_suffix = self::get_pro_suffix($full_name);
     if ($professional_suffix) {
       # Remove the suffix from full name
       $full_name = str_replace($professional_suffix, '', $full_name);
       # Remove the preceeding comma and space(s) from suffix
       $professional_suffix = preg_replace("/, */", '', $professional_suffix);
       # Normalize the case of suffix if found in dictionary
-      foreach ($this->dict['suffixes']['prof'] as $prosuffix) {
+      foreach (self::$dict['suffixes']['prof'] as $prosuffix) {
         if (strtolower($prosuffix) === strtolower($professional_suffix)) {
           $professional_suffix = $prosuffix;
         }
       }
     }
     # Deal with nickname, push to array
-    $has_nick = $this->get_nickname($full_name);
+    $has_nick = self::get_nickname($full_name);
     if ($has_nick) {
       # Remove wrapper chars from around nickname
       $name['nickname'] = substr($has_nick, 1, (strlen($has_nick) - 2));
@@ -72,15 +72,15 @@ class NameParser {
       $full_name = str_replace('  ', ' ', $full_name);
     }
     # Grab a list of words from name
-    $unfiltered_name_parts = $this->break_words($full_name);
+    $unfiltered_name_parts = self::break_words($full_name);
     # Is first word a title or multiple titles consecutively?
-    while ($s = $this->is_salutation($unfiltered_name_parts[0])) {
+    while ($s = self::is_salutation($unfiltered_name_parts[0])) {
       $salutation .= "$s ";
       array_shift($unfiltered_name_parts);
     }
     $salutation = trim($salutation);
     # Is last word a suffix or multiple suffixes consecutively?
-    while ($s = $this->is_suffix($unfiltered_name_parts[count($unfiltered_name_parts)-1], $full_name)) {
+    while ($s = self::is_suffix($unfiltered_name_parts[count($unfiltered_name_parts)-1], $full_name)) {
       $suffix .= "$s ";
       array_pop($unfiltered_name_parts);
     }
@@ -98,18 +98,18 @@ class NameParser {
       $word = $unfiltered_name_parts[$i];
       # move on to parsing the last name if we find an indicator of a compound last name (Von, Van, etc)
       # we use $i != 0 to allow for rare cases where an indicator is actually the first name (like "Von Fabella")
-      if ($this->is_compound($word) && $i != 0) {
+      if (self::is_compound($word) && $i != 0) {
         break;
       }
       # is it a middle initial or part of their first name?
       # if we start off with an initial, we'll call it the first name
-      if ($this->is_initial($word)) {
+      if (self::is_initial($word)) {
         # is the initial the first word?
         if ($i == 0) {
           # if so, do a look-ahead to see if they go by their middle name
           # for ex: "R. Jason Smith" => "Jason Smith" & "R." is stored as an initial
           # but "R. J. Smith" => "R. Smith" and "J." is stored as an initial
-          if ($this->is_initial($unfiltered_name_parts[$i+1])) {
+          if (self::is_initial($unfiltered_name_parts[$i+1])) {
             $fname .= " ".strtoupper($word);
           }
           else {
@@ -122,19 +122,19 @@ class NameParser {
         }
       }
       else {
-        $fname .= " ".$this->fix_case($word);
+        $fname .= " ".self::fix_case($word);
       }
     }
     # check that we have more than 1 word in our string
     if ($end-0 > 1) {
       # concat the last name
       for ($i; $i < $end; $i++) {
-        $lname .= " ".$this->fix_case($unfiltered_name_parts[$i]);
+        $lname .= " ".self::fix_case($unfiltered_name_parts[$i]);
       }
     }
     else {
       # otherwise, single word strings are assumed to be first names
-      $fname = $this->fix_case($unfiltered_name_parts[$i]);
+      $fname = self::fix_case($unfiltered_name_parts[$i]);
     }
     # return the various parts in an array
     $name['salutation'] = $salutation;
@@ -150,7 +150,7 @@ class NameParser {
    * @param string $name the full name you wish to parse
    * @return array full list of words broken down by spaces
    */
-  public function break_words($name) {
+  public static function break_words($name) {
     return explode(' ', $name);
   }
   /**
@@ -159,8 +159,8 @@ class NameParser {
    * @param string $name the name you wish to test
    * @return mixed returns the suffix if exists, false otherwise
    */
-  protected function get_pro_suffix($name) {
-    foreach ($this->dict['suffixes']['prof'] as $suffix) {
+  protected static function get_pro_suffix($name) {
+    foreach (self::$dict['suffixes']['prof'] as $suffix) {
       if (preg_match("/,[\s]*$suffix\b/i", $name, $matches)) {
         return $matches[0];
       }
@@ -179,7 +179,7 @@ class NameParser {
    * @param string $name the name you wish to test against
    * @return mixed returns nickname if exists, false otherwise
    */
-  protected function get_nickname($name) {
+  protected static function get_nickname($name) {
     if (preg_match("/[\(|\"].*?[\)|\"]/", $name, $matches)) {
       return $matches[0];
     }
@@ -192,22 +192,22 @@ class NameParser {
    * @param string $name full name for context in determining edge-cases
    * @return mixed boolean if false, string if true (returns suffix)
    */
-  protected function is_suffix($word, $name) {
+  protected static function is_suffix($word, $name) {
     # Ignore periods, normalize case
     $word = str_replace('.', '', strtolower($word));
     # Search the array for our word
-    $line_match = array_search($word, array_map('strtolower', $this->dict['suffixes']['line']));
-    $prof_match = array_search($word, array_map('strtolower', $this->dict['suffixes']['prof']));
+    $line_match = array_search($word, array_map('strtolower', self::$dict['suffixes']['line']));
+    $prof_match = array_search($word, array_map('strtolower', self::$dict['suffixes']['prof']));
     # Break out for professional suffix matches first
     if ($prof_match !== false) {
-      return $this->dict['suffixes']['prof'][$prof_match];
+      return self::$dict['suffixes']['prof'][$prof_match];
     }
     # Now test our edge cases based on lineage
     if ($line_match !== false) {
       # Store our match
-      $matched_case = $this->dict['suffixes']['line'][$line_match];
+      $matched_case = self::$dict['suffixes']['line'][$line_match];
       # Remove it from the array
-      $temp_array = $this->dict['suffixes']['line'];
+      $temp_array = self::$dict['suffixes']['line'];
       unset($temp_array[$line_match]);
       # Make sure we're dealing with the suffix and not a surname
       if ($word == 'senior' || $word == 'junior') {
@@ -234,9 +234,9 @@ class NameParser {
    * @param string $word the single word you wish to test
    * @return boolean
    */
-  protected function is_salutation($word) {
+  protected static function is_salutation($word) {
     $word = str_replace('.', '', strtolower($word));
-    foreach ($this->dict['prefix'] as $replace => $originals) {
+    foreach (self::$dict['prefix'] as $replace => $originals) {
       if (in_array($word, $originals)) {
         return $replace;
       }
@@ -249,8 +249,8 @@ class NameParser {
    * @param string $word the single word you wish to test
    * @return boolean
    */
-  protected function is_compound($word) {
-    return array_search(strtolower($word), $this->dict['compound']);
+  protected static function is_compound($word) {
+    return array_search(strtolower($word), self::$dict['compound']);
   }
   /**
    * Test string to see if it's a single letter/initial (period optional)
@@ -258,7 +258,7 @@ class NameParser {
    * @param string $word the single word you wish to test
    * @return boolean
    */
-  protected function is_initial($word) {
+  protected static function is_initial($word) {
     return ((strlen($word) == 1) || (strlen($word) == 2 && $word{1} == "."));
   }
   /**
@@ -267,7 +267,7 @@ class NameParser {
    * @param string $word the single word you wish to test
    * @return boolean
    */
-  protected function is_camel_case($word) {
+  protected static function is_camel_case($word) {
     if (preg_match("/[A-Za-z]([A-Z]*[a-z][a-z]*[A-Z]|[a-z]*[A-Z][A-Z]*[a-z])[A-Za-z]*/", $word)) {
       return true;
     }
@@ -275,14 +275,14 @@ class NameParser {
   }
   # ucfirst words split by dashes or periods
   # ucfirst all upper/lower strings, but leave camelcase words alone
-  public function fix_case($word) {
+  public static function fix_case($word) {
     # Fix case for words split by periods (J.P.)
     if (strpos($word, '.') !== false) {
-      $word = $this->safe_ucfirst(".", $word);;
+      $word = self::safe_ucfirst(".", $word);;
     }
     # Fix case for words split by hyphens (Kimura-Fay)
     if (strpos($word, '-') !== false) {
-      $word = $this->safe_ucfirst("-", $word);
+      $word = self::safe_ucfirst("-", $word);
     }
     # Special case for single letters
     if (strlen($word) == 1) {
@@ -291,19 +291,19 @@ class NameParser {
     # Special case for 2-letter words
     if (strlen($word) == 2) {
       # Both letters vowels (uppercase both)
-      if (in_array(strtolower($word{0}), $this->dict['vowels']) && in_array(strtolower($word{1}), $this->dict['vowels'])) {
+      if (in_array(strtolower($word{0}), self::$dict['vowels']) && in_array(strtolower($word{1}), self::$dict['vowels'])) {
         $word = strtoupper($word);
       }
       # Both letters consonants (uppercase both)
-      if (!in_array(strtolower($word{0}), $this->dict['vowels']) && !in_array(strtolower($word{1}), $this->dict['vowels'])) {
+      if (!in_array(strtolower($word{0}), self::$dict['vowels']) && !in_array(strtolower($word{1}), self::$dict['vowels'])) {
         $word = strtoupper($word);
       }
       # First letter is vowel, second letter consonant (uppercase first)
-      if (in_array(strtolower($word{0}), $this->dict['vowels']) && !in_array(strtolower($word{1}), $this->dict['vowels'])) {
+      if (in_array(strtolower($word{0}), self::$dict['vowels']) && !in_array(strtolower($word{1}), self::$dict['vowels'])) {
         $word = ucfirst(strtolower($word));
       }
       # First letter consonant, second letter vowel or "y" (uppercase first)
-      if (!in_array(strtolower($word{0}), $this->dict['vowels']) && (in_array(strtolower($word{1}), $this->dict['vowels']) || strtolower($word{1}) == 'y')) {
+      if (!in_array(strtolower($word{0}), self::$dict['vowels']) && (in_array(strtolower($word{1}), self::$dict['vowels']) || strtolower($word{1}) == 'y')) {
         $word = ucfirst(strtolower($word));
       }
     }
@@ -314,11 +314,11 @@ class NameParser {
     return $word;
   }
   # helper public function for fix_case
-  public function safe_ucfirst($seperator, $word) {
+  public static function safe_ucfirst($seperator, $word) {
     # uppercase words split by the seperator (ex. dashes or periods)
     $parts = explode($seperator, $word);
     foreach ($parts as $word) {
-      $words[] = ($this->is_camel_case($word)) ? $word : ucfirst(strtolower($word));
+      $words[] = (self::is_camel_case($word)) ? $word : ucfirst(strtolower($word));
     }
     return implode($seperator, $words);
   }
